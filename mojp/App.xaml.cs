c5f880d45f00dfe92eac0b285865ef51d4aac7c5
fog-bank <ogawa-yoshitaka@outlook.jp>
2016-10-10 12:48:15 +0900

    初期コード

---
+++ mojp/App.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace Mojp
+{
+	public partial class App : Application
+	{
+		private static readonly Dictionary<string, Card> cards = new Dictionary<string, Card>();
+
+		/// <summary>
+		/// カードの英語名から、英語カード名・日本語カード名・日本語カードテキストを検索します。
+		/// </summary>
+		public static Dictionary<string, Card> Cards
+		{
+			get { return cards; }
+		}
+
+		public static void SaveAsXml(string path)
+		{
+			var cardsElem = new XElement("cards");
+
+			foreach (var card in cards.Values)
+				cardsElem.Add(card.ToXml());
+
+			var doc = new XDocument(new XDeclaration("1.0", "utf-8", "yes"), cardsElem);
+			doc.Save(path);
+		}
+
+		public static void SetCardInfosFromWhisper(StreamReader sr)
+		{
+			cards.Clear();
+
+			foreach (var card in Card.ParseWhisper(sr))
+				cards.Add(card.Name, card);
+		}
+
+		public static void SetCardInfosFromXml(XDocument doc)
+		{
+			cards.Clear();
+
+			foreach (var xml in doc.Descendants("card"))
+			{
+				var card = Card.FromXml(xml);
+
+				cards.Add(card.Name, card);
+			}
+		}
+
+		public static void SetCardInfosFromXml(string file)
+		{
+			SetCardInfosFromXml(XDocument.Load(file));
+		}
+	}
+}
---
+++ mojp/OptionDialog.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using Microsoft.Win32;
+
+namespace Mojp
+{
+	public partial class OptionDialog : Window
+	{
+		public OptionDialog(object vm)
+		{
+			InitializeComponent();
+
+			var fonts = Fonts.SystemFontFamilies;
+			var fontNames = new List<string>(fonts.Count);
+
+			foreach (var font in fonts)
+				fontNames.Add(font.Source);
+
+			fontNames.Sort();
+			this.cmbFonts.ItemsSource = fontNames;
+
+			this.DataContext = vm;
+		}
+
+		private void OnBrowseSearchTxt(object sender, RoutedEventArgs e)
+		{
+			var dlg = new OpenFileDialog();
+			dlg.FileName = "search.txt";
+			dlg.CheckFileExists = true;
+			dlg.Filter = "すべてのファイル (*.*)|*.*";
+
+			if (dlg.ShowDialog(this) == true)
+			{
+				using (var stream = dlg.OpenFile())
+				using (var sr = new StreamReader(stream, Encoding.GetEncoding("shift-jis")))
+				{
+					App.SetCardInfosFromWhisper(sr);
+
+					if (cbxSaveDb.IsChecked == true)
+						App.SaveAsXml("cards.xml");
+				}
+			}
+		}
+	}
+}
