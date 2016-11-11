6c2d92d5bb093e7a8762722c6d8e3e251c23e152
fog-bank <fog-bank@users.noreply.github.com>
2016-11-11 21:36:08 +0900

    キーワード処理・生成にすべて対応

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -54,4 +54,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.11111.10")]
+[assembly: AssemblyFileVersion("1.2.11111.11")]
