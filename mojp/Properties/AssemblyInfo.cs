d4d7286a1b9bd66fa3f5bbfb43da31eae4b7af56
fog-bank <fog-bank@users.noreply.github.com>
2016-11-19 00:57:33 +0900

    Automationイベント処理の見直し、UIのMargin見直し、カードデータの更新反映

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -55,4 +55,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.11119.20")]
+[assembly: AssemblyFileVersion("1.2.11119.21")]
