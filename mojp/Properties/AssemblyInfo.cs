5b36589c4d2dd1da545c0e88cda4ae6e256682d9
fog-bank <fog-bank@users.noreply.github.com>
2016-12-03 18:36:54 +0900

    残りのヴァンガードを追加

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -55,4 +55,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.11203.24")]
+[assembly: AssemblyFileVersion("1.2.11203.25")]
