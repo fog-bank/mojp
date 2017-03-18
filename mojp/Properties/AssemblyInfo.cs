317b00bce9735c2c1bd8f76842646242e7cf18a7
fog-bank <ogawa-yoshitaka@outlook.jp>
2017-03-18 13:42:05 +0900

    育殻組のヴォレルのテキストと掘葬の儀式のwikiリンクを更新

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.20309.15")]
+[assembly: AssemblyFileVersion("1.3.20318.16")]
