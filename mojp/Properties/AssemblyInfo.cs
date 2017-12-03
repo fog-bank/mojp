704005572d652586b6b4b19cf965c7edcdb753b2
fog-bank <fog-bank@users.noreply.github.com>
2017-12-03 23:55:41 +0900

    テンプレートの変更：is the number of ... on the battlefield

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.21202.52")]
+[assembly: AssemblyFileVersion("1.6.21203.53")]
