4e0ea5f8fa13c7f00a51c8e4e51a493a2eea7075
fog-bank <fog-bank@users.noreply.github.com>
2018-07-04 17:38:46 +0900

    M19の新録カードを追加

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.30704.2")]
+[assembly: AssemblyFileVersion("2.0.30704.3")]
