3a872358864bc58e3bc901dab6ffbce6475ec6fa
fog-bank <fog-bank@users.noreply.github.com>
2018-08-08 14:58:49 +0900

    Edit the raw csproj file (remove unused property)

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.30725.19")]
+[assembly: AssemblyFileVersion("2.0.30808.20")]
