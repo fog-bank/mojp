63a6458c215d01021106e186baa2e309680204f2
fog-bank <fog-bank@users.noreply.github.com>
2019-02-03 22:20:10 +0900

    WHISPERの小更新を反映

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.3.0.0")]
-[assembly: AssemblyFileVersion("2.3.40126.12")]
+[assembly: AssemblyFileVersion("2.3.40203.13")]
