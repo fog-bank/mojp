f61eaf698fad5728324a22d80b9aad22f232bdaa
fog-bank <fog-bank@users.noreply.github.com>
2019-03-22 14:15:07 +0900

    テンプレートの変更：あなたの対戦相手がコントロールしていて → 対戦相手がコントロールしていて

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.3.0.0")]
-[assembly: AssemblyFileVersion("2.3.40322.20")]
+[assembly: AssemblyFileVersion("2.3.40322.21")]
