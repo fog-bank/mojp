b15185fac011f2d2eb3818d430bd578822e6761e
fog-bank <fog-bank@users.noreply.github.com>
2019-06-29 10:28:41 +0900

    M20新カードを追加

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.5.0.0")]
-[assembly: AssemblyFileVersion("2.5.40607.2")]
+[assembly: AssemblyFileVersion("2.6.40629.2")]
