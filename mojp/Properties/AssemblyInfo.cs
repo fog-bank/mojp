69a67b56f999efbb1226e817c4b2305649bd995c
fog-bank <fog-bank@users.noreply.github.com>
2019-07-01 18:19:17 +0900

    テンプレートの変更：on each Plant creature you control (がコントロールする各クリーチャーの上に → がコントロールしている各クリーチャーの上に)

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.6.0.0")]
-[assembly: AssemblyFileVersion("2.6.40701.9")]
+[assembly: AssemblyFileVersion("2.6.40701.10")]
