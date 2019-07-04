8ce388871045bb233a76c9e8bcaf9fbd2034a97f
fog-bank <fog-bank@users.noreply.github.com>
2019-07-04 15:29:36 +0900

    Core Set 2020 Oracle Changes 対応 (宝物トークンなど)

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.6.0.0")]
-[assembly: AssemblyFileVersion("2.6.40703.14")]
+[assembly: AssemblyFileVersion("2.6.40704.14")]
