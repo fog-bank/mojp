edccad63150ac1a1dd54781d33609d6dfc92ecec
fog-bank <fog-bank@users.noreply.github.com>
2019-09-25 14:49:37 +0900

    サブタイプ更新（貴族、邪術師）

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.7.0.0")]
-[assembly: AssemblyFileVersion("2.7.40925.0")]
+[assembly: AssemblyFileVersion("2.7.40925.1")]
