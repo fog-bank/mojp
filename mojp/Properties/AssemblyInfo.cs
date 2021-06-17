cdb1d47ff990ba7392b2c2bf89c8e1332af157c1
fog-bank <fog-bank@users.noreply.github.com>
2021-06-17 19:39:00 +0900

    テンプレートの反転：すべての土地 (続) / すべての未消費のマナ

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.15.0.0")]
-[assembly: AssemblyFileVersion("2.15.10615.1")]
+[assembly: AssemblyFileVersion("2.15.10617.1")]
