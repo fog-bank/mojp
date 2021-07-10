b5047805d6f9bb5b88bc70e805bd298c8baaed3d
fog-bank <fog-bank@users.noreply.github.com>
2021-07-10 21:58:06 +0900

    《不浄なる暗黒の書》のオラクル修正および《ターシャズ・ヒディアス・ラフター》のテキスト修正

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.16.0.0")]
-[assembly: AssemblyFileVersion("2.16.10709.1")]
+[assembly: AssemblyFileVersion("2.16.10710.1")]
