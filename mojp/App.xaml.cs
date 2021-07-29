6c8755f4c67b4cd574fd9764cc89c6a990ab60e4
fog-bank <fog-bank@users.noreply.github.com>
2021-07-29 09:19:32 +0900

    AFCの《王国まといの巨人》に対応

--- mojp/App.xaml.cs
+++ mojp/App.xaml.cs
@@ -38,7 +38,7 @@ namespace Mojp
         /// <summary>
         /// 代替テキストによるカード検索を行います。
         /// </summary>
-        public static Dictionary<string, AltCard> AltCards { get; } = new Dictionary<string, AltCard>(442);
+        public static Dictionary<string, AltCard> AltCards { get; } = new Dictionary<string, AltCard>(443);
 
         /// <summary>
         /// このアプリの設定を取得します。
