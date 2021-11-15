856dd1706b7223a0da01d13073349aa7a091d087
fog-bank <fog-bank@users.noreply.github.com>
2021-11-15 11:30:03 +0900

    テンプレートの変更：任意のマナ

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.18.0.0")]
-[assembly: AssemblyFileVersion("2.18.11111.0")]
+[assembly: AssemblyFileVersion("2.18.11115.1")]
