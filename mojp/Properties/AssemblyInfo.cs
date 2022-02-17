4712d157d519b87303a14d6aa5fdbcf7b205ebde
fog-bank <fog-bank@users.noreply.github.com>
2022-02-18 04:49:16 +0900

    Street Fighter x Magic: The Gathering Secret Lair Cards

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -54,4 +54,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.19.0.0")]
-[assembly: AssemblyFileVersion("2.19.20210.0")]
+[assembly: AssemblyFileVersion("2.19.20218.1")]
