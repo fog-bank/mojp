110184c78792cc07d769a09202759e001b15227b
fog-bank <fog-bank@users.noreply.github.com>
2022-05-06 13:44:52 +0900

    テンプレート変更：対戦相手１人の墓地にある１枚

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -54,4 +54,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.20.0.0")]
-[assembly: AssemblyFileVersion("2.20.20504.1")]
+[assembly: AssemblyFileVersion("2.20.20506.1")]
