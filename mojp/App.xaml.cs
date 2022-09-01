48024be91e2cde1c6c6b955a9a191fc7290dc2f4
fog-bank <fog-bank@users.noreply.github.com>
2022-09-01 12:18:59 +0900

    《Sol'kanar the Tainted》のカード名違い / 基本土地のセット記号修正

--- mojp/App.xaml.cs
+++ mojp/App.xaml.cs
@@ -38,7 +38,7 @@ namespace Mojp
         /// <summary>
         /// 代替テキストによるカード検索を行います。
         /// </summary>
-        public static Dictionary<string, AltCard> AltCards { get; } = new Dictionary<string, AltCard>(2111);
+        public static Dictionary<string, AltCard> AltCards { get; } = new Dictionary<string, AltCard>(2112);
 
         /// <summary>
         /// このアプリの設定を取得します。
