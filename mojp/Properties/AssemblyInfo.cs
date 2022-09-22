ff4c19150b5929d096940c6bae2f9655b5c17971
fog-bank <fog-bank@users.noreply.github.com>
2022-09-22 15:16:28 +0900

    テンプレート変更：Put one of them into your hand and the other into ~

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -54,4 +54,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.22.0.0")]
-[assembly: AssemblyFileVersion("2.22.20921.1")]
+[assembly: AssemblyFileVersion("2.22.20922.1")]
