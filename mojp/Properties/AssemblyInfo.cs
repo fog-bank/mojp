dab3c2de188743f41e4d93ab0c075c03e6c465b7
fog-bank <fog-bank@users.noreply.github.com>
2023-04-16 13:07:50 +0900

    英雄譚とバトルの注釈文を削除

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -54,4 +54,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.25.0.0")]
-[assembly: AssemblyFileVersion("2.25.30414.0")]
+[assembly: AssemblyFileVersion("2.25.30416.1")]
