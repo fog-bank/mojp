401fba9b19d6932aa9ee56cee6c448f9017b7a06
fog-bank <fog-bank@users.noreply.github.com>
2023-09-17 11:05:34 +0900

    WHISPERと照合 / SLX 追加対応 / ____ Goblin 更新対応

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -54,4 +54,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.28.0.0")]
-[assembly: AssemblyFileVersion("2.28.30831.0")]
+[assembly: AssemblyFileVersion("2.28.30917.1")]
