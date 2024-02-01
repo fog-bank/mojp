68d9a654bd45633b485e40b003c85457a3a1dbb5
fog-bank <fog-bank@users.noreply.github.com>
2024-02-01 13:19:16 +0900

    プロモ対応

--- mojp/App.xaml.cs
+++ mojp/App.xaml.cs
@@ -23,7 +23,7 @@ namespace Mojp
         /// <summary>
         /// カードの英語名から、英語カード名・日本語カード名・日本語カードテキストを検索します。
         /// </summary>
-        public static Dictionary<string, Card> Cards { get; } = new Dictionary<string, Card>(26981);
+        public static Dictionary<string, Card> Cards { get; } = new Dictionary<string, Card>(27349);
 
         /// <summary>
         /// 代替テキスト検索の主キーです。
@@ -38,7 +38,7 @@ namespace Mojp
         /// <summary>
         /// 代替テキストによるカード検索を行います。
         /// </summary>
-        public static Dictionary<string, AltCard> AltCards { get; } = new Dictionary<string, AltCard>(5277);
+        public static Dictionary<string, AltCard> AltCards { get; } = new Dictionary<string, AltCard>(5523);
 
         /// <summary>
         /// このアプリの設定を取得します。
@@ -263,16 +263,19 @@ namespace Mojp
         /// <param name="processName">プロセスの名前。</param>
         public static Process GetProcessByName(string processName)
         {
-            Process targetProc = null;
+            var procs = Process.GetProcessesByName(processName);
+            return procs?.FirstOrDefault();
 
-            foreach (var proc in Process.GetProcesses())
-            {
-                if (targetProc == null && string.Equals(proc.ProcessName, processName, StringComparison.OrdinalIgnoreCase))
-                    targetProc = proc;
-                else
-                    proc.Dispose();
-            }
-            return targetProc;
+            //Process targetProc = null;
+
+            //foreach (var proc in Process.GetProcesses())
+            //{
+            //    if (targetProc == null && string.Equals(proc.ProcessName, processName, StringComparison.OrdinalIgnoreCase))
+            //        targetProc = proc;
+            //    else
+            //        proc.Dispose();
+            //}
+            //return targetProc;
         }
 
         protected override void OnStartup(StartupEventArgs e)
