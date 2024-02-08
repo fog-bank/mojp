058c412e45dba73e9ec5b298f17e79ee4aef7c32
fog-bank <fog-bank@users.noreply.github.com>
2024-02-08 10:37:00 +0900

    変装・偽装用のヘルパーカードテキストを削除

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -54,4 +54,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.30.0.0")]
-[assembly: AssemblyFileVersion("2.30.40201.0")]
+[assembly: AssemblyFileVersion("2.30.40208.1")]
