93c5d8f41a2f6367906a63070cce8c41cd243a0c
fog-bank <fog-bank@users.noreply.github.com>
2024-06-02 19:26:34 +0900

    MH3

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -53,5 +53,5 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.31.0.0")]
-[assembly: AssemblyFileVersion("2.31.40510.1")]
+[assembly: AssemblyVersion("2.32.0.0")]
+[assembly: AssemblyFileVersion("2.32.40602.0")]
