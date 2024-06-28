338f17033310f257d58c822ac1e7b194689eb8e8
fog-bank <fog-bank@users.noreply.github.com>
2024-06-28 17:13:02 +0900

    ACR対応、MH3 special guest 対応漏れ修正

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -54,4 +54,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.32.0.0")]
-[assembly: AssemblyFileVersion("2.32.40618.1")]
+[assembly: AssemblyFileVersion("2.32.40628.2")]
