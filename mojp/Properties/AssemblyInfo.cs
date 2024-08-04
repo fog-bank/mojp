13cce3546ddd7675405a544ac8a5a378c8e47c40
fog-bank <fog-bank@users.noreply.github.com>
2024-08-04 17:25:47 +0900

    WHISPERと照合 / (Ｐ) → (獣痕) / squirrels

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -54,4 +54,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.33.0.0")]
-[assembly: AssemblyFileVersion("2.33.40725.0")]
+[assembly: AssemblyFileVersion("2.33.40804.1")]
