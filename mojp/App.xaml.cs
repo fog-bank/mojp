9c24ad5032f5601685883e170a79a49c20ccb9de
fog-bank <fog-bank@users.noreply.github.com>
2024-09-14 15:01:59 +0900

    DSK

--- mojp/App.xaml.cs
+++ mojp/App.xaml.cs
@@ -23,7 +23,7 @@ public partial class App : Application
     /// <summary>
     /// カードの英語名から、英語カード名・日本語カード名・日本語カードテキストを検索します。
     /// </summary>
-    public static Dictionary<string, Card> Cards { get; } = new(28430);
+    public static Dictionary<string, Card> Cards { get; } = new(28568);
 
     /// <summary>
     /// 代替テキスト検索の主キーです。
@@ -38,7 +38,7 @@ public partial class App : Application
     /// <summary>
     /// 代替テキストによるカード検索を行います。
     /// </summary>
-    public static Dictionary<string, AltCard> AltCards { get; } = new(6548);
+    public static Dictionary<string, AltCard> AltCards { get; } = new(6561);
 
     /// <summary>
     /// このアプリの設定を取得します。
--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -53,5 +53,5 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.33.0.0")]
-[assembly: AssemblyFileVersion("2.33.40815.2")]
+[assembly: AssemblyVersion("2.34.0.0")]
+[assembly: AssemblyFileVersion("2.34.40919.0")]
