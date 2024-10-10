46bcae372e4e12fca0ff4ed9a2c18a3d99dab2c7
fog-bank <fog-bank@users.noreply.github.com>
2024-10-10 18:10:59 +0900

    PIP 復活 / テンプレート緩和：as long as ~ turn,

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -54,4 +54,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.34.0.0")]
-[assembly: AssemblyFileVersion("2.34.41006.1")]
+[assembly: AssemblyFileVersion("2.34.41010.2")]
