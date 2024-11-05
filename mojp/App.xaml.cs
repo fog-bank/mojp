5f1e4d64c277c052dec87de1675e6c8c5f8156d3
fog-bank <fog-bank@users.noreply.github.com>
2024-11-05 15:05:55 +0900

    FDN追加

--- mojp/App.xaml.cs
+++ mojp/App.xaml.cs
@@ -23,7 +23,7 @@ public partial class App : Application
     /// <summary>
     /// カードの英語名から、英語カード名・日本語カード名・日本語カードテキストを検索します。
     /// </summary>
-    public static Dictionary<string, Card> Cards { get; } = new(28721);
+    public static Dictionary<string, Card> Cards { get; } = new(28912);
 
     /// <summary>
     /// 代替テキスト検索の主キーです。
@@ -38,7 +38,7 @@ public partial class App : Application
     /// <summary>
     /// 代替テキストによるカード検索を行います。
     /// </summary>
-    public static Dictionary<string, AltCard> AltCards { get; } = new(7265);
+    public static Dictionary<string, AltCard> AltCards { get; } = new(7275);
 
     /// <summary>
     /// このアプリの設定を取得します。
