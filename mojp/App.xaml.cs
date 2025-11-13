d474c3a0dc0ddd380bfb278923a11f37d73732b6
fog-bank <fog-bank@users.noreply.github.com>
2025-11-13 15:25:21 +0900

    v3.5.0

--- mojp/App.xaml.cs
+++ mojp/App.xaml.cs
@@ -24,7 +24,7 @@ public partial class App : Application
     /// <summary>
     /// カードの英語名から、英語カード名・日本語カード名・日本語カードテキストを検索します。
     /// </summary>
-    public static Dictionary<string, Card> Cards { get; } = new(30851 + 164);
+    public static Dictionary<string, Card> Cards { get; } = new(30853 + 164);
 
     /// <summary>
     /// このアプリの設定を取得します。
