6a491a7a08095f5eec9ed8e146a8622a3cc6c2da
fog-bank <fog-bank@users.noreply.github.com>
2026-01-22 19:40:16 +0900

    WHISPERと照合

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -54,4 +54,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.6.0.0")]
-[assembly: AssemblyFileVersion("3.6.10115.0")]
+[assembly: AssemblyFileVersion("3.6.10122.1")]
