2add8f78ad8c44010b3dfc71c42a2678d7afe1ad
fog-bank <fog-bank@users.noreply.github.com>
2026-03-01 00:08:17 +0900

    融合の注釈文を更新

--- mojp/Properties/AssemblyInfo.cs
+++ mojp/Properties/AssemblyInfo.cs
@@ -54,4 +54,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.7.0.0")]
-[assembly: AssemblyFileVersion("3.7.10228.0")]
+[assembly: AssemblyFileVersion("3.7.10301.0")]
